e69381be4a97e9ecc62fdb66f205fe1ea9fddf59
olonafihla <olonafihla@gmail.com>
2023-02-05 19:36:16 +0200

    using turtle moduke

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('pyCharm')
+timmy = Turtle()
+my_screen = Screen()
+print(my_screen.canvheight)
+timmy.shape("turtle")
+timmy.color("red", "green ")
+timmy.forward(100)
+my_screen.exitonclick()
